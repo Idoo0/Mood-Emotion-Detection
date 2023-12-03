fd4cc8e36abff9080f467875436f0e157e910ced
Idooo <ridhoaditya925@gmail.com>
2023-12-03 21:42:29 +0800

    menghubungakan model voice

--- app.py
+++ app.py
@@ -1,16 +1,43 @@
 from flask import Flask, render_template, request, redirect, url_for
-import os
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+import os
 import numpy as np
+import librosa
+import soundfile
+import glob, pickle
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'static/uploadFoto'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+UPLOAD_VOICE = 'static/uploadVoice'
+app.config['UPLOAD_VOICE'] = UPLOAD_VOICE
 
 # Load your pre-trained model (replace 'path/to/your/model.h5' with your actual model path)
 model = load_model('model_gambar.h5')
+model_voice = pickle.load(open("modelForPrediction1.sav", 'rb'))
+
+def extract_feature(file_name, mfcc, chroma, mel):
+    with soundfile.SoundFile(file_name) as sound_file:
+        X = sound_file.read(dtype="float32")
+        sample_rate=sound_file.samplerate
+        if chroma:
+            stft=np.abs(librosa.stft(X))
+        result=np.array([])
+        if mfcc:
+            mfccs=np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
+            result=np.hstack((result, mfccs))
+        if chroma:
+            chroma=np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
+            result=np.hstack((result, chroma))
+        if mel:
+            mel=np.mean(librosa.feature.melspectrogram(y=X, sr=sample_rate).T,axis=0)
+            result=np.hstack((result, mel))
+    return result
 
 def predict_image(image_path):
     img = image.load_img(image_path, target_size=(48, 48), grayscale=True)
@@ -41,8 +68,16 @@ def gambar():
 
     return render_template('gambar.html')
 
-@app.route('/suara')
+@app.route('/suara',  methods=['GET', 'POST'])
 def suara():
+    if request.method == 'POST':
+        if 'voice' in request.files:
+            file = request.files['voice']
+            if file.filename != '':
+                filepath = os.path.join(app.config['UPLOAD_VOICE'], file.filename)
+                file.save(filepath)
+                return redirect(url_for('resultVoice', filename=file.filename))
+
     return render_template('suara.html')
 
 @app.route('/resultFoto/<filename>')
@@ -60,9 +95,13 @@ def resultFoto(filename):
     marah = f"{probability[3]*100:.2f}";
     return render_template('resultFoto.html', filename=filename, predicted_category=predicted_category, confidence=confidence, sedih=sedih, senang=senang, marah=marah, netral=netral)
 
-@app.route('/resultVoice')
-def resultVoice():
-    return render_template('resultVoice.html')
+@app.route('/resultVoice/<filename>')
+def resultVoice(filename):
+    voice_path = os.path.join(app.config['UPLOAD_VOICE'], filename)
+    feature=extract_feature(voice_path, mfcc=True, chroma=True, mel=True)
+    feature=feature.reshape(1,-1)
+    prediction=model_voice.predict(feature)
+    return render_template('resultVoice.html', filename=filename, prediction=prediction)
 
 if __name__ == "__main__":
     app.run(debug=True)
