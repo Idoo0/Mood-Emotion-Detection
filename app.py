bc7d88b927ff38fd4df926bd27fd52ba1f3e3aa7
Idooo <ridhoaditya925@gmail.com>
2023-12-03 16:52:52 +0800

    menghubungkan dengan model

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'uploadFoto'
+UPLOAD_FOLDER = 'static/uploadFoto'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Load your pre-trained model (replace 'path/to/your/model.h5' with your actual model path)
+model = load_model('model_gambar.h5')
+
+def predict_image(image_path):
+    img = image.load_img(image_path, target_size=(48, 48), grayscale=True)
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array /= 255.0
+
+    prediction = model.predict(img_array)
+    return prediction
 
 @app.route('/')
 def index():
@@ -17,15 +32,11 @@ def home2():
 @app.route('/gambar', methods=['GET', 'POST'])
 def gambar():
     if request.method == 'POST':
-        # Memastikan bahwa 'gambar' adalah nama field pada form
         if 'gambar' in request.files:
             file = request.files['gambar']
             if file.filename != '':
-                # Menggabungkan path upload dengan nama file
                 filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-                # Menyimpan file ke folder uploadFoto
                 file.save(filepath)
-                # Redirect ke halaman resultFoto.html atau halaman lain yang diinginkan
                 return redirect(url_for('resultFoto', filename=file.filename))
 
     return render_template('gambar.html')
@@ -34,13 +45,24 @@ def gambar():
 def suara():
     return render_template('suara.html')
 
-@app.route('/resultFoto')
-def resultFoto():
-    return render_template('resultFoto.html')
+@app.route('/resultFoto/<filename>')
+def resultFoto(filename):
+    image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    predictions = predict_image(image_path)
+    categories = ['Sedih', 'Senang', 'Netral', 'Marah']
+    predicted_class = np.argmax(predictions)
+    predicted_category = categories[predicted_class]
+    confidence = predictions[0, predicted_class]
+    probability = predictions[0];
+    sedih = f"{probability[0]*100:.2f}";
+    senang = f"{probability[1]*100:.2f}";
+    netral = f"{probability[2]*100:.2f}";
+    marah = f"{probability[3]*100:.2f}";
+    return render_template('resultFoto.html', filename=filename, predicted_category=predicted_category, confidence=confidence, sedih=sedih, senang=senang, marah=marah, netral=netral)
 
 @app.route('/resultVoice')
 def resultVoice():
     return render_template('resultVoice.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
