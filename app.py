6a3e1ef87df302f6785bc66f6de43ec20e09ed70
reginagusalim <reginagussalim08@gmail.com>
2023-12-03 15:02:57 +0800

    file berhasil tersimpan di folder

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploadFoto'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -10,8 +14,20 @@ def index():
 def home2():
     return render_template('home2.html')
 
-@app.route('/gambar')
+@app.route('/gambar', methods=['GET', 'POST'])
 def gambar():
+    if request.method == 'POST':
+        # Memastikan bahwa 'gambar' adalah nama field pada form
+        if 'gambar' in request.files:
+            file = request.files['gambar']
+            if file.filename != '':
+                # Menggabungkan path upload dengan nama file
+                filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+                # Menyimpan file ke folder uploadFoto
+                file.save(filepath)
+                # Redirect ke halaman resultFoto.html atau halaman lain yang diinginkan
+                return redirect(url_for('resultFoto', filename=file.filename))
+
     return render_template('gambar.html')
 
 @app.route('/suara')
@@ -26,5 +42,5 @@ def resultFoto():
 def resultVoice():
     return render_template('resultVoice.html')
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run()
