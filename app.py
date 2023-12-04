0bf6758b7204886125e2a32c62ed4dbc7bafc8be
Idooo <ridhoaditya925@gmail.com>
2023-12-04 08:56:40 +0800

    finishing

--- app.py
+++ app.py
@@ -107,5 +107,9 @@ def resultVoice(filename):
 def project():
     return render_template('project.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
